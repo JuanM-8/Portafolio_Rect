34fde8a48dd6cee1edea6073a0f08dce129f7942
Juan Marin <juandavidmarin.0008@gmail.com>
2025-05-20 21:19:42 -0500

    A new certificate

--- src/components/Certificados.tsx
+++ src/components/Certificados.tsx
@@ -12,6 +12,7 @@ const certificadosData = [
   { img: "media/certificados/CMTJS.webp", titulo: "2024 - Generacion TIC", year: 2024 },
   { img: "media/certificados/CMTIA.webp", titulo: "2024 - Generacion TIC", year: 2024 },
   { img: "media/certificados/CssCertificado.jpeg", titulo: "2024 - SoloLearn", year: 2024 },
+  { img: "media/certificados/CGenerativeAI.webp", titulo: "2025 - Coursera", year: 2025 }
 ];
 
 function Certificados() {
