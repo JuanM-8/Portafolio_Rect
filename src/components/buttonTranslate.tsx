7fcf8877104b33cab0f98b929da0f80696207dcf
Juan Marin <juandavidmarin.0008@gmail.com>
2025-06-02 14:22:34 -0500

    Updated

--- src/components/buttonTranslate.tsx
+++ src/components/buttonTranslate.tsx
@@ -1,19 +1,18 @@
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+
 import "../Styles/Button.css";
 function Button() {
-  const [isSpanish, setIsSpanish] = useState(false);
   const { i18n } = useTranslation();
+  const isSpanish = i18n.language === "es";
 
-  const changeLanguage = (lng: string) => {
-    setIsSpanish(lng === "es");
-    i18n.changeLanguage(lng);
+  const changeLanguage = () => {
+    const newLang = isSpanish ? "en" : "es";
+    i18n.changeLanguage(newLang);
   };
+
   return (
     <div className="buttonLanguage">
-      <button  onClick={() => changeLanguage(isSpanish ? "en" : "es")}>
-        {isSpanish ? "EN" : "ES"}
-      </button>
+      <button onClick={changeLanguage}>{isSpanish ? "EN" : "ES"}</button>
     </div>
   );
 }
